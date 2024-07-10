4cb6c1dc4888200f9e9999e7d9e2ce032935e3cb
Joao Gabriel Chiarello <joao.chiarello@angellira.com.br>
2024-07-10 16:19:58 -0300

    SharedPreferences na tela de cadastro

--- app/src/main/java/com/angellira/petvital1/CadastroActivity.kt
+++ app/src/main/java/com/angellira/petvital1/CadastroActivity.kt
@@ -3,6 +3,7 @@ package com.angellira.petvital1
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -21,43 +22,51 @@ class CadastroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        binding = ActivityCadastroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         val botaoRegisto = findViewById<Button>(R.id.BotaoRegistrar)
-        botaoRegisto.setOnClickListener{
+        botaoRegisto.setOnClickListener {
 
             val cadastro = User()
-            val intent = (Intent(this, CadastroActivity::class.java))
 
             cadastro.email = findViewById<EditText>(R.id.textoregistroEmail).text.toString()
             cadastro.password = findViewById<EditText>(R.id.passwordEditText).text.toString()
             cadastro.username = findViewById<EditText>(R.id.usernameEditText).text.toString()
 
-            val estaRegistrado = registerUser()
+            if (ValidacaoInput(cadastro.username, cadastro.email, cadastro.password)) {
+                val user = User(cadastro.username, cadastro.email, cadastro.password)
+                registerUser(user)
 
-            if(estaRegistrado){
-                val intent = Intent(this, LoginActivity::class.java)
+                val intent = (Intent(this, CadastroActivity::class.java))
                 startActivity(intent)
                 finish()
-            }else{
-                Toast.makeText(this, "Cadastro Falhou, tente novamente.", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Por favor, preencha todos os campos.", Toast.LENGTH_SHORT)
+                    .show()
             }
-
         }
 
-
-
-        binding = ActivityCadastroBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.BotaoRegistrar.setOnClickListener{
+        binding.BotaoRegistrar.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
         }
 
     }
-    private fun registerUser(cadastro: User): Boolean {
+
+    private fun ValidacaoInput(username: String, email: String, password: String): Boolean {
+        return username.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()
+
+    }
+
+    private fun registerUser(cadastro: User) {
 
         val sharedPreferences = getPreferences(Context.MODE_PRIVATE)
-        cadastro.email
-
-        return true
+        with(sharedPreferences.edit()) {
+            putString("email", cadastro.email)
+            putString("senha", cadastro.password)
+            putString("usuario", cadastro.username)
+            apply()
+        }
+        Log.d("RegisterActivity", "Usuário registrado: $cadastro")
     }
 }
--- app/src/main/java/com/angellira/petvital1/LoginActivity.kt
+++ app/src/main/java/com/angellira/petvital1/LoginActivity.kt
@@ -2,37 +2,50 @@ package com.angellira.petvital1
 
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.angellira.petvital1.Interfaces.autenticator
 import com.angellira.petvital1.databinding.ActivityLoginBinding
+import com.angellira.petvital1.model.User
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    val cadastrado = User()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        val sharedPreferences = getPreferences(Context.MODE_PRIVATE)
-
-
+        JaestaLogado()
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         val intent = Intent(this, MainActivity::class.java)
 
+        val botaoLogin = findViewById<Button>(R.id.botaoLogin)
+        botaoLogin.setOnClickListener{
 
-        binding.botaoLogin.setOnClickListener {
-            val email = binding.textEmailLogin.text.toString()
-            intent.putExtra("email", email)
+            cadastrado.email = findViewById<EditText>(R.id.textEmailLogin).text.toString()
+            cadastrado.password = findViewById<EditText>(R.id.editTextPassword).text.toString()
+
+            if(validateLogin(cadastrado.email, cadastrado.password)){
+
+            }
+
+            val nomeusuario = binding.textEmailLogin.text.toString()
+            intent.putExtra("Usuario", cadastrado.username)
             startActivity(intent)
         }
 
-        binding.BotaoRegistrar.setOnClickListener{
+        binding.BotaoRegistrar.setOnClickListener {
             startActivity(Intent(this, CadastroActivity::class.java))
         }
 
@@ -40,4 +53,30 @@ class LoginActivity : AppCompatActivity() {
             startActivity(Intent(this, EsqueciASenha::class.java))
         }
     }
+
+
+    private fun JaestaLogado() {
+        val sharedPreferences = getPreferences(MODE_PRIVATE)
+
+        val estaLogado = b(sharedPreferences)
+
+        if (estaLogado) {
+            val main = Intent(this, MainActivity::class.java)
+            startActivity(main)
+        }
+
+        sharedPreferences.edit().putBoolean("Logou", true).apply()
+    }
+
+    private fun validateLogin(email: String, password: String): Boolean {
+        if(cadastrado.email == email && cadastrado.password == password){
+            return true
+        }else{
+            return false
+        }
+    }
+
+    private fun b(sharedPreferences: SharedPreferences) =
+        sharedPreferences.getBoolean("Logou", false)
+
 }
