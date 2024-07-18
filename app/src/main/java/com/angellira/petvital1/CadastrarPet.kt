c3a6825502a8f574a4a053f9c52359e5331d9a0c
JoaoChi <148506329+JoaoChi@users.noreply.github.com>
2024-07-18 18:13:26 -0300

    Botão cadastrar pet

--- app/src/main/java/com/angellira/petvital1/CadastrarPet.kt
+++ app/src/main/java/com/angellira/petvital1/CadastrarPet.kt
@@ -1,35 +1,37 @@
 package com.angellira.petvital1
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.angellira.petvital1.databinding.ActivityLoginBinding
-
+import com.angellira.petvital1.databinding.ActivityCadastrarPetBinding
 
 class CadastrarPet : AppCompatActivity() {
 
-    private lateinit var binding: ActivityLoginBinding
+    private lateinit var binding: ActivityCadastrarPetBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        binding = ActivityLoginBinding.inflate(layoutInflater)
+        binding = ActivityCadastrarPetBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setContentView(R.layout.activity_cadastrar_pet)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
-            binding.BotaoRegistrar.setOnClickListener{
-                startActivity(this, MainActivity::class.java)
-            }
         }
+
+        botaoRegistrar()
     }
 
-
+    private fun botaoRegistrar() {
+        binding.buttonSalvarPet.setOnClickListener {
+        startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
 }
+
