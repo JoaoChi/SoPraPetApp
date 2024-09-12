8cfb664da7927d76225641e86266b9705b1a1533
Joao Gabriel Chiarello <joao.chiarello@angellira.com.br>
2024-09-12 16:07:34 -0300

    Adicionei 2 dropdown menu com list, para quando forem pressionados abrirem a lista de datas e serviços do petshop, pretendo colocar algo como calendario no local dos horários no futuro.

--- app/src/main/java/com/angellira/petvital1/AgendaActivity.kt
+++ app/src/main/java/com/angellira/petvital1/AgendaActivity.kt
@@ -7,16 +7,28 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuBoxScope
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -36,7 +48,10 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.angellira.petvital1.model.Pet
+import com.angellira.petvital1.model.Petshop
 import com.angellira.petvital1.ui.theme.PetVital1Theme
+import kotlin.math.exp
 
 class AgendaActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,8 +67,16 @@ class AgendaActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun app() {
+
+    val options = listOf("Banho", "Tosa", "Veterinária")
+    var expanded by remember { mutableStateOf(false) }
+    var expandir by remember { mutableStateOf(false) }
+    val horarios = listOf("1, 2, 3, 4, 5, 6, 7, 8, 9, 10")
+    var selectedText by remember { mutableStateOf("Selecione o serviço") }
+    var selectedText2 by remember { mutableStateOf("Selecione o horário") }
 
     var valorCampo by remember {
         mutableStateOf("")
@@ -74,6 +97,18 @@ fun app() {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
+                text = "SóPraPet",
+                style = TextStyle(
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.W900,
+                    color = Color(0xFFFFC107)
+                )
+            )
+
+            Spacer(modifier = Modifier.height(80.dp))
+
+
+            Text(
                 text = "Qual Serviço Gostaria \nde agendar?",
                 style = TextStyle(
                     color = Color(255, 255, 255, 255),
@@ -93,9 +128,9 @@ fun app() {
                     "Tosa"
                 }
 
-                val cor = if(gasolina){
+                val cor = if (gasolina) {
                     Color.Red
-                }else{
+                } else {
                     Color.Green
                 }
 
@@ -108,64 +143,128 @@ fun app() {
                 )
             }
 
+            ExposedDropdownMenuBox(
+                expanded = expanded,
+                onExpandedChange = { expanded = !expanded },
+            ) {
+                TextField(
+                    value = selectedText,
+                    onValueChange = {},
+                    readOnly = true,
+                    label = { Text("Selecione o serviço") },
+                    modifier = Modifier
+                        .clickable { expanded = true },
+                    trailingIcon = {
+                        ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+                    },
+                    colors = ExposedDropdownMenuDefaults.textFieldColors()
+                )
 
-            TextField(value = valorCampo2,
-                onValueChange = {
-                    valorCampo2 = it
-                },
-                label = {
-                    Text(text = "Selecione o serviço")
-                }
 
-            )
-
-            TextField(value = valorCampo,
-                onValueChange = {
-                    valorCampo = it
-                },
-                label = {
-                    Text(text = "Selecione a data")
-                }
-            )
-        }
-
-        Spacer(modifier = Modifier.height(20.dp))
-
-        Row {
-            Image(
-                painter = painterResource(R.drawable.dia_mundial_do_gato__veterin_rio_cat_felinos_fevereiro_divertido_moderno_post_de_instagram),
-                contentDescription = "Perfil Usuario",
-                modifier = Modifier
-                    .size(40.dp)
-                    .clip(CircleShape)
-            )
-
-            Spacer(modifier = Modifier.width(8.dp))
-
-            Column {
-                Text(
-                    text = "Agende agora",
-                    style = TextStyle(
-                        color = Color(255, 255, 255, 255),
-                        fontSize = 16.sp,
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false },
+                    modifier = Modifier.width(150.dp)
+                ) {
+                    options.forEach { option ->
+                        DropdownMenuItem(
+                            text = { Text(option) },
+                            onClick = {
+                                selectedText = option
+                                expanded = false
+                            }
                         )
-                )
-
-                Spacer(modifier = Modifier.height(4.dp))
-
-                Text(
-                    text = "Estamos lhe esperando :)",
-                    style = TextStyle(
-                        color = Color(255,255,255,255),
-                        fontSize = 16.sp,
-                    )
-
-                )
+                    }
+                }
             }
 
+            ExposedDropdownMenuBox(expanded = expandir,
+                onExpandedChange = { expandir = !expandir }
+            )
+            {
+                TextField(
+                    value = selectedText2,
+                    onValueChange = {},
+                    readOnly = true,
+                    label = {
+                        Text(text = "Selecione a data")
+                    },
+                    modifier = Modifier
+                        .clickable { expandir = true },
+                    trailingIcon = {
+                        ExposedDropdownMenuDefaults.TrailingIcon(expanded = expandir)
+                    },
+                    colors = ExposedDropdownMenuDefaults.textFieldColors()
+                )
+                DropdownMenu(expanded = expandir,
+                    onDismissRequest = { expandir = false },
+                    modifier = Modifier.width(150.dp)
+                ) {
+                    horarios.forEach { horarios ->
+                        DropdownMenuItem(text = { Text(horarios) },
+                            onClick = {
+                                selectedText2 = horarios
+                                expandir = false
+                            })
+
+
+                    }
+
+                }
+            }
+
+
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Row {
+                Box(
+                    modifier = Modifier
+                        .size(40.dp)
+                        .clip(CircleShape)
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.dia_mundial_do_gato__veterin_rio_cat_felinos_fevereiro_divertido_moderno_post_de_instagram),
+                        contentDescription = "Perfil Usuario",
+                        modifier = Modifier.fillMaxSize()
+                    )
+
+                    Icon(
+                        imageVector = Icons.Filled.Check,
+                        contentDescription = "Icone Check",
+                        tint = Color.Green,
+                        modifier = Modifier
+                            .align(Alignment.BottomEnd)
+                            .padding(4.dp)
+
+                    )
+
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+
+                Column {
+
+
+                    Text(
+                        text = "Petchopp",
+                        style = TextStyle(
+                            color = Color(255, 255, 255, 255),
+                            fontSize = (16.sp)
+                        )
+                    )
+                    Text(
+                        text = "Petshop Verificado!",
+                        style = TextStyle(
+                            color = Color(255, 255, 255, 255),
+                            fontSize = (16.sp)
+                        )
+                    )
+                }
+            }
         }
     }
 }
+
 
 @Preview
 @Composable
